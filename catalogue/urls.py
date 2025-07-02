07c53feb45cb7ee776e798eacea66100f9540df7
Sharanya Chatterjee <sharanyac08@gmail.com>
2025-07-02 18:07:48 -0400

    HTML templates created, landing page implemented

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('earrings/', views.earring_list, name='earring_list'),
+    path('earrings/', views.earrings_list, name='earrings_list'),
     path('earrings/upload/', views.earring_upload, name='earring_upload'),
     
-    path('tops/', views.top_list, name='top_list'),
+    path('tops/', views.tops_list, name='tops_list'),
     path('tops/upload/', views.top_upload, name='top_upload'),
     
-    path('bottoms/', views.bottom_list, name='bottom_list'),
-    path('bottoms/upload/', views.bottom_upload, name='bottom_upload')
+    path('bottoms/', views.bottoms_list, name='bottoms_list'),
+    path('bottoms/upload/', views.bottom_upload, name='bottom_upload'),
+    
+    path('', views.home, name='home')
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -4,42 +4,45 @@ from .forms import EarringForm, TopForm, BottomForm
 
 def earrings_list(request):
     earrings = Earring.objects.all()
-    return render(request, "catalogue/earring_list.html", {"earrings": earrings})
+    return render(request, "catalogue/earrings_list.html", {"earrings": earrings})
 
 def earring_upload(request):
     if request.method == "POST":
         form = EarringForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect("earring_list")
+            return redirect("earrings_list")
     else:
         form = EarringForm()
     return render(request, "catalogue/earring_upload.html", {"form": form})
 
 def tops_list(request):
     tops = Top.objects.all()
-    return render(request, "catalogue/top_list.html", {"tops": tops})
+    return render(request, "catalogue/tops_list.html", {"tops": tops})
 
 def top_upload(request):
     if request.method == "POST":
         form = TopForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect("top_list")
+            return redirect("tops_list")
     else:
         form = TopForm()
     return render(request, "catalogue/top_upload.html", {"form": form})
 
 def bottoms_list(request):
     bottoms = Bottom.objects.all()
-    return render(request, "catalogue/bottom_list.html", {"bottoms": bottoms})
+    return render(request, "catalogue/bottoms_list.html", {"bottoms": bottoms})
 
 def bottom_upload(request):
     if request.method == "POST":
         form = BottomForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect("bottom_list")
+            return redirect("bottoms_list")
     else:
         form = BottomForm()
     return render(request, "catalogue/bottom_upload.html", {"form": form})
+
+def home(request):
+    return render(request, "catalogue/home.html")
